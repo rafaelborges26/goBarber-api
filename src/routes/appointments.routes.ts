48879d1552ae6aac95775b597687d577365506aa
= <rafael.borges2698@gmail.com>
2020-07-21 02:48:11 -0300

    adding the routes of appointments

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,17 +1,33 @@
 import { Router, request, response } from 'express'
 import { uuid } from 'uuidv4'
+import { startOfHour, parseISO, isEqual } from 'date-fns'
+
+interface Appointment {
+    id: string
+    provider: string
+    date: Date
+}
 
 const appointmentsRouter = Router()
 
-const appointments = []
+const appointments: Appointment[] = []
 
 appointmentsRouter.post('/', (request, response ) => {
     const { provider, date } = request.body
 
+    const parsedDate = startOfHour(parseISO(date))
+
+    const findAppointmentsInSameDate = appointments.find(appointment => isEqual(parsedDate, appointment.date))
+
+    if(findAppointmentsInSameDate) {
+        return response.status(404).json({message: 'this appointment has been already booked'})
+    }
+
+
     const appointment = {
         id: uuid(),
         provider,
-        date
+        date: parsedDate
     }
 
     appointments.push(appointment)
