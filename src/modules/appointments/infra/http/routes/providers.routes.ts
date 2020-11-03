1c8a798986ba36690e1c4dee9753321109d699e7
= <rafael.borges2698@gmail.com>
2020-11-03 00:32:59 -0300

    add the controllers and routes from avaiability day/month

--- src/modules/appointments/infra/http/routes/providers.routes.ts
+++ src/modules/appointments/infra/http/routes/providers.routes.ts
@@ -2,13 +2,24 @@ import { Router, request, response } from 'express'
 
 import ensureAuthenticated from '@modules/users/middlewares/ensureAuthenticated'
 import ProvidersController from '../controllers/ProvidersController'
+import ProviderDayAvailabilityController from '../controllers/ProviderDayAvailabilityController'
+import ProviderMonthAvailabilityController from '../controllers/ProviderMonthAvailabilityController'
+
+
 
 const providersRouter = Router()
 
 const providersController = new ProvidersController()
+const providerDayAvailabilityController = new ProviderDayAvailabilityController()
+const providerMonthAvailabilityController = new ProviderMonthAvailabilityController()
+
+
 
 providersRouter.use(ensureAuthenticated)
 
+providersRouter.get('/:provider_id/month-availability', providerMonthAvailabilityController.index)
+
+providersRouter.get('/:provider_id/day-availability', providerDayAvailabilityController.index)
 
 //appointmentsRouter.get('/', async (request, response )=> {
 //    console.log(request.user)
