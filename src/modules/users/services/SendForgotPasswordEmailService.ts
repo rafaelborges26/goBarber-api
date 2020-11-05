15a75714c2b77ca05ca9ddcb0e2c28fe382fbb77
= <rafael.borges2698@gmail.com>
2020-11-05 14:30:50 -0300

    add environment variables

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -48,7 +48,7 @@ class SendForgotPasswordEmailService {
                 file: forgotPasswordTemplate,
                 variables: { //essa variavel vai ser preenchida no {{name}} do template
                     name: user.name,
-                    link: `http://localhost:3000/reset_password?token=${token}`
+                    link: `${process.env.APP_WEB_URL}/reset_password?token=${token}`
                 }
             },
 
