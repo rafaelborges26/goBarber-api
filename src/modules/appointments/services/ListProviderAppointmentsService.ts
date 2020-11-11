17e41620bda1781e65cd46b89d4742fa2e281015
= <rafael.borges2698@gmail.com>
2020-11-11 15:06:29 -0300

    add cache with redis

--- src/modules/appointments/services/ListProviderAppointmentsService.ts
+++ src/modules/appointments/services/ListProviderAppointmentsService.ts
@@ -1,6 +1,7 @@
 import { inject, injectable } from 'tsyringe'
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository'
 import Appointment from '../infra/typeorm/entities/Appointment'
+import ICacheProvider from '@shared/container/providers/CacheProvider/models/ICacheProvider'
 
 interface Request {
     provider_id: string
@@ -13,16 +14,27 @@ interface Request {
 class ListProviderAppointmentsService {
     constructor(
         @inject('AppointmentsRepository')
-        private appointmentsRepository: IAppointmentsRepository
+        private appointmentsRepository: IAppointmentsRepository,
+
+        @inject('CacheProvider')
+        private cacheProvider: ICacheProvider
     ) {}
 
     public async execute( {provider_id, year, month, day }:Request ): Promise<Appointment[]> {
+
+        //list redis
+        const cacheData = await this.cacheProvider.recover('ooi')
+
+        console.log(cacheData)
+
         const appointments = await this.appointmentsRepository.findAllInDayFromProvider({
             provider_id,
             year,
             month,
             day
         })
+
+        //await this.cacheProvider.save('ooi', 'ooi')
 
         return appointments
     }
