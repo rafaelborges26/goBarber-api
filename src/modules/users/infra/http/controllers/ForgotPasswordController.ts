f2afe55931fbe023b08eca54f433d2a9e6d10060
= <rafael.borges2698@gmail.com>
2021-01-20 17:24:01 -0300

    adding the build and adding the ormconfigexample

--- src/modules/users/infra/http/controllers/ForgotPasswordController.ts
+++ src/modules/users/infra/http/controllers/ForgotPasswordController.ts
@@ -1,7 +1,7 @@
 import {Request, Response} from 'express'
 import { container } from 'tsyringe'
 
-import SendForgotPasswordEmailService from '@modules/users/services/SendForgotPasswordEmailService.ts'
+import SendForgotPasswordEmailService from '@modules/users/services/SendForgotPasswordEmailService'
 
 
 export default class ForgotPasswordController {
